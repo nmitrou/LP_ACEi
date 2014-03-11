d9ecd3ce1672d4f0d923425615617ce3a30cadae
Nick Mitrou <nmitrou@sfu.ca>
2014-03-11 11:51:36 -0700

    First commit

---
+++ FRC/Matlab_codes/LP_ACEi_datacollect.m
@@ -0,0 +1,130 @@
+%% Load LP-ACEi RBF-BP and export as text
+% load files and rename
+%% export text files
+% 12H01
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H01/2012H01_HR_25Hz_2ms_3_signal_results.mat');
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H01_SPN.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H01/2012H01_HR_25Hz_2ms_4_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H01_LP.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H01/2012H01_HR_25Hz_2ms_5_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H01_CAPT.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H01/2012H01_HR_25Hz_2ms_6_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H01_LPCAPT.txt','rbfbp','-ascii')
+
+clear TIV_rbfbp
+clear TIV_lsbp
+clear COH
+clear f
+clear TFgain
+clear TFphase
+clear rbfbp
+clear ls
+
+%% 12H02
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H02/2012H02_HR_25Hz_2ms_3_signal_results.mat');
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H02_SPN.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H02/2012H02_HR_25Hz_2ms_4_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H02_LP.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H02/2012H02_HR_25Hz_2ms_5_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H02_CAPT.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H02/2012H02_HR_25Hz_2ms_6_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H02_LPCAPT.txt','rbfbp','-ascii')
+
+clear TIV_rbfbp
+clear TIV_lsbp
+clear COH
+clear f
+clear TFgain
+clear TFphase
+clear rbfbp
+clear ls
+%% 12H03
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H03/2012H03_HR_25Hz_2ms_3_signal_results.mat');
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H03_SPN.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H03/2012H03_HR_25Hz_2ms_4_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H03_LP.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H03/2012H03_HR_25Hz_2ms_5_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H03_CAPT.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H03/2012H03_HR_25Hz_2ms_6_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H03_LPCAPT.txt','rbfbp','-ascii')
+
+clear TIV_rbfbp
+clear TIV_lsbp
+clear COH
+clear f
+clear TFgain
+clear TFphase
+clear rbfbp
+clear ls
+%% 12H07
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H07/2012H07_HR_25Hz_2ms_3_signal_results.mat');
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H07_SPN.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H07/2012H07_HR_25Hz_2ms_4_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H07_LP.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H07/2012H07_HR_25Hz_2ms_5_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H07_CAPT.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H07/2012H07_HR_25Hz_2ms_6_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H07_LPCAPT.txt','rbfbp','-ascii')
+
+clear TIV_rbfbp
+clear TIV_lsbp
+clear COH
+clear f
+clear TFgain
+clear TFphase
+clear rbfbp
+clear ls
+%% 12H10
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H10/2012H10_HR_25Hz_2ms_3_signal_results.mat');
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H10_SPN.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H10/2012H10_HR_25Hz_2ms_4_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H10_LP.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H10/2012H10_HR_25Hz_2ms_5_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H10_CAPT.txt','rbfbp','-ascii')
+
+load('/Users/nickmitrou/Documents/SFU/PhD/Data/LP-ACEi-FRC/capt_results/2012H10/2012H10_HR_25Hz_2ms_6_signal_results.mat')
+
+save('/Users/nickmitrou/Documents/SFU/PhD/Projects/LP-ACEi/FRC/Data/RBFBP_text/12H10_LPCAPT.txt','rbfbp','-ascii')
+
+clear TIV_rbfbp
+clear TIV_lsbp
+clear COH
+clear f
+clear TFgain
+clear TFphase
+clear rbfbp
+clear ls
